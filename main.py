46a17df368dfb0f35c103aa851df5b258e7f89c6
Rafal Gajdulewicz <r.gajdulewicz@gmail.com>
2017-01-26 14:03:15 +0100

    Add job fetching

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from __future__ import unicode_literals
 
 import youtube_dl
+import time
+import requests
+import json
 
 ydlv = youtube_dl.YoutubeDL(
-        {'outtmpl': u'%(title)s.%(ext)s', "restrictfilenames": True,'format': 'mp4', 
-         'postprocessors': [{
-             'key': 'FFmpegVideoConvertor',
-             'preferedformat': 'mp4',
-         }]})
+    {'outtmpl': u'%(title)s.%(ext)s', "restrictfilenames": True, 'format': 'mp4',
+     'postprocessors': [{
+         'key': 'FFmpegVideoConvertor',
+         'preferedformat': 'mp4',
+     }]})
 ydla = youtube_dl.YoutubeDL(
-        {'outtmpl': u'%(title)s.%(ext)s', "restrictfilenames": True,'format': 'bestaudio/best', 
-         'postprocessors': [{
-             'key': 'FFmpegExtractAudio',
-             'preferredcodec': 'mp3',
-             'preferredquality': '192',
-         }]})
+    {'outtmpl': u'%(title)s.%(ext)s', "restrictfilenames": True, 'format': 'bestaudio/best',
+     'postprocessors': [{
+         'key': 'FFmpegExtractAudio',
+         'preferredcodec': 'mp3',
+         'preferredquality': '192',
+     }]})
+
 
 def download(video_url, audio=False):
     if audio:
@@ -23,11 +27,25 @@ def download(video_url, audio=False):
         enc = ydlv
     with enc:
         result = enc.extract_info(video_url, download=True)
-        print result
+
+
+def lease():
+    r = requests.post("http://scari.herokuapp.com/jobs/lease")
+    if r.status_code == 204:
+        return None
+    lease = json.loads(r.text)
+    return lease
 
 
 def main():
-    download('https://www.youtube.com/watch?v=9xQp2sldyts')
+    while True:
+        r = lease()
+        if not r:
+            print "Sleeping"
+            time.sleep(5)
+            continue
+        print r
+        download(r['job']['Source'])
 
 
 if __name__ == "__main__":
